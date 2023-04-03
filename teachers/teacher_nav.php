10ee714223b508201d1a6707b9cf33a5ab537655
Suuuuu <99704389+MrSudayon@users.noreply.github.com>
2023-04-03 21:08:52 +0800

    git update

---
+++ teachers/teacher_nav.php
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <a href="tchr_dashboard.php"><li><img src="../images/dashboard (2).png" alt="">&nbsp;&nbsp;&nbsp; <h4 class="menu-text">Dashboard</h4></li></a>
+         <!--<a href="tchr_attendance.php"><li><img src="../images/teacher2.png" alt="">&nbsp;&nbsp;&nbsp; <h4 class="menu-text">Attendance</h4></li></a>-->
+        <a href="modules.php"><li><img src="../images/reading-book (1).png" alt="">&nbsp;&nbsp;&nbsp; <h4 class="menu-text">Modules</h4></li></a>
+        <a href="links.php"><li><img src="../images/link.png" alt="">&nbsp;&nbsp;&nbsp; <h4 class="menu-text">Bookmarks</h4></li></a>
+        <a href="student_list.php"><li><img src="../images/reading-book (1).png" alt="">&nbsp;&nbsp;&nbsp; <h4 class="menu-text">List of Sections</h4></li></a>
+        <a href="grades.php"><li><img src="../images/grade.png" alt="">&nbsp;&nbsp;&nbsp; <h4 class="menu-text">Students Grades</h4></li></a>
+        <a href="settings.php"><li><img src="../images/settings.png" alt="">&nbsp;&nbsp;&nbsp; <h4 class="menu-text">Settings</h4></li></a>
+    </ul>
+</nav>
